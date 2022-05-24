49814dbef583dd0b8d27ab4f034af91b86cd85d7
Atsushi <atsushi@moongift.jp>
2022-05-24 10:56:05 +0900

    Init

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,5 @@
 const $ = Dom7;
 
-// Login Screen Demo
-$('#my-login-screen .login-button').on('click', function () {
-  var username = $('#my-login-screen [name="username"]').val();
-  var password = $('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
-});
-
 // NCMBの初期化用
 const event = window.cordova ? 'deviceready' : 'DOMContentLoaded';
 document.addEventListener(event, async (e) => {
